cb52215e2c3b3f04c22a9361e200803517938c85
jumperM4 <pma1994ars@yandex.ru>
2023-07-05 22:51:51 +0300

    cart item component styling + add cart item func + cart icon count func

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,14 +7,14 @@ import { ReactComponent as ShoppingIcon } from "../../assets/114 shopping-bag.sv
 import { CartContext } from "../../context/cart.context";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
