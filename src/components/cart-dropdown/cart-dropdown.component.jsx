a4f34b0d9be536e68d08c01670a138ceb12e5642
jumperM4 <pma1994ars@yandex.ru>
2023-07-06 21:32:23 +0300

    creating checkout page + decrement\increment checkout item quantity

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../context/cart.context";
 
 import "./cart-dropdown.styles.scss";
@@ -9,6 +11,12 @@ import CartItem from "../cart-item/cart-item.component";
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
 
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -16,7 +24,7 @@ const CartDropdown = () => {
           return <CartItem key={item.id} cartItem={item} />;
         })}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
