93bed1b822f4e220e0577a90a842beea11f4989a
jumperM4 <pma1994ars@yandex.ru>
2023-07-12 20:51:24 +0300

    routing of directory using useNavigate

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,13 +1,19 @@
 import "./directory-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
     <div
       className="directory-item-container"
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
+      onClick={onNavigateHandler}
     >
       <div className="background-image" />
       <div className="directory-item-body">
