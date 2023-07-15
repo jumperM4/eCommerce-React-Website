94db849c73c77f45f8a1d2c96d91c226c7ed742c
jumperM4 <pma1994ars@yandex.ru>
2023-07-15 13:36:00 +0300

    installing redux + setting up the store

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { compose, applyMiddleware } from "redux";
+import { legacy_createStore as createStore } from "redux";
+
+import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+//----------------------------------------------------------------
+const middleWares = [logger];
+composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
