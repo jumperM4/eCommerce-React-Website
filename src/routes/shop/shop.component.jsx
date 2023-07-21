3f81a36dbdc84dcfe742689af7a1dfcc3d07215e
jumperM4 <pma1994ars@yandex.ru>
2023-07-21 15:45:04 +0300

    migrating data logic from firebase to selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,7 +6,7 @@ import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 import "./shop.styles.scss";
 
@@ -15,8 +15,9 @@ const Shop = () => {
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments("categories");
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      console.log(categoriesArray);
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
